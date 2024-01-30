5559b907a09cb69ed968002c0e36596406c12caa
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2024-01-30 13:14:03 -0300

    feat: Configuração Docker -> Postgres | Criação de usuario e database (dw | dl)

---
+++ docker_airflow/docker_postgres_init_dl.sql
@@ -0,0 +1,2 @@
+CREATE USER etl_airflow_dl WITH PASSWORD 'password_etl_airflow_dl' SUPERUSER CREATEDB;
+CREATE DATABASE moedas_dl;
---
+++ docker_airflow/docker_postgres_init_dw.sql
@@ -0,0 +1,2 @@
+CREATE USER etl_airflow_dw WITH PASSWORD 'password_etl_airflow_dw' CREATEDB;
+CREATE DATABASE moedas_dw;
